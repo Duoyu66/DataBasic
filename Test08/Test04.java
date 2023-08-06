b0cb6ff12b7559c82c441bc45d5e5c0a0dc4ce12
lbl <2523890936@qq.com>
2023-08-06 17:43:38 +0800

    数组相关练习

---
+++ Test08/Test04.java
@@ -0,0 +1,44 @@
+package Test08;
+
+import java.util.Arrays;
+
+public class Test04 {
+    public static void main(String[] args) {
+        Test04 test04 = new Test04();
+        int[] arr = new int[]{4, 6, 2, 3, 8, 6, 10, 1};
+        System.out.println(Arrays.toString(test04.Maopao(arr)));
+
+    }
+
+    //冒泡
+    public static int[] Maopao(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    //二分排序练习
+    public int search(int[] nums, int target) {
+        int left = 0,right = nums.length-1;
+
+        while (left<=right){
+            int mid = (right-left)/2+right;
+            if(nums[mid]==target){
+                return mid;
+            } else if (nums[mid]>target) {
+                right = mid -1 ;
+            }else{
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
